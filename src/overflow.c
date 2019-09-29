d25fed3cab5535a840ff6ab81d5d2f70ea8b99cb
Trenton Stiles <stiles.trenton@gmail.com>
2019-09-30 02:57:46 +0430

    clean up code, add comments

--- src/overflow.c
+++ src/overflow.c
@@ -21,6 +21,7 @@ char * printAlphabet(int length, char character, int alphabetCount)
 	/* total of characters added to the buffer */
 	sum = 0;
 
+	/* in intervals of alphabetCount we add 4 of each letter to the buffer */
 	for(i = 0; i < interval; i++){
 		for(j = 0; j < alphabetCount; j++){
 			buff[ (i * alphabetCount) + j] = character;
@@ -29,6 +30,7 @@ char * printAlphabet(int length, char character, int alphabetCount)
 		character++;
 	}
 
+	/* fills the rest of the buffer */
 	for(i = sum ; i < length; i++){
 		buff[i] = character;
 		sum++;
@@ -40,13 +42,10 @@ char * printAlphabet(int length, char character, int alphabetCount)
 /* Basic function to print a set length of character to stdout */
 char * printString(int length, char character)
 {
-	int i;
 	char *buff;
 
 	buff = calloc(length + 1, sizeof(char));
-	for(i = 0; i < length; i++){
-		buff[i] = character;
-	}
+	memset(buff, character, sizeof(char) * length);
 
-	return buff; // return 0 for now
+	return buff;
 }
